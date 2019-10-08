b32906c6b67c2356525ab11d36529702075f540f
Tomek <tomasz0zwierzynski@gmail.com>
2019-10-08 21:38:42 +0200

    Dodano REST do kolekcji Users

--- index.js
+++ index.js
@@ -18,6 +18,14 @@ app.get('/', (request, response) => {
 
 app.get('/users', db.getUsers);
 
+app.get('/users/:id', db.getUserById);
+
+app.post('/users', db.createUser);
+
+app.put('/users/:id', db.updateUser);
+
+app.delete('/users/:id', db.deleteUser);
+
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 });
--- src/queries.js
+++ src/queries.js
@@ -7,15 +7,74 @@ const pool = new Pool({
   port: 5432,
 });
 
+// TODO: dodawanie użytkownika
+
+// TODO: odczytywanie budżetów
+// TODO: odczytywanie subskrypcji dla konkretnego użytkownika
+// TODO: odczytywanie pozycji dla konkretnego budżetu dla konkretnego użytkownika
+
 const getUsers = (request, response) => {
     pool.query('SELECT * FROM users ORDER BY id ASC', (error, results) => {
       if (error) {
-        throw error
+        throw error;
       }
-      response.status(200).json(results.rows)
+      response.status(200).json(results.rows);
+    });
+};
+
+const getUserById = (request, response) => {
+    const id = parseInt(request.params.id);
+
+    pool.query('SELECT * FROM users WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).json(results.rows);
+    });
+}
+
+const createUser = (request, response) => {
+    const { username, password, display } = request.body;
+  
+    pool.query('INSERT INTO users (username, password, display) VALUES ($1, $2, $3) RETURNING id', [username, password, display], (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).send(`User added with ID: ${results.rows[0].id}`);
+    });
+};
+
+const updateUser = (request, response) => {
+    const id = parseInt(request.params.id);
+    const { username, password, display } = request.body;
+  
+    pool.query(
+      'UPDATE users SET username = $1, password = $2, display = $3 WHERE id = $4',
+      [username, password, display, id],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        response.status(200).send(`User modified with ID: ${id}`);
+      }
+    )
+};
+
+const deleteUser = (request, response) => {
+    const id = parseInt(request.params.id);
+  
+    pool.query('DELETE FROM users WHERE id = $1', [id], (error, results) => {
+      if (error) {
+        throw error;
+      }
+      response.status(200).send(`User deleted with ID: ${id}`);
     })
 };
 
 module.exports = {
-    getUsers
+    getUsers,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser
 }

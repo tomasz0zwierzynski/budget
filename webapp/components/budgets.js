66a873595a01d7d3081c579a13955a225b624b3f
tzwierzy <T.Zwierzynski@wasko.pl>
2019-10-15 16:03:03 +0200

    dodano font awesome

--- webapp/components/budgets.js
+++ webapp/components/budgets.js
@@ -1,5 +1,5 @@
 Vue.component('budget-position', {
-    template: `
+    template: /*html*/ `
         <tr>
             <th scope="w-auto row">
                 <div class="form-check m-1">
@@ -11,18 +11,22 @@ Vue.component('budget-position', {
                 <div class="input-group input-group-sm" v-if="labelEdit">
                     <input ref="labelInput" type="text" class="form-control form-control-sm" v-if="labelEdit" v-on:keyup.enter="labelProtoOkClick()" v-model="labelProto">
                     <div class="input-group-append">
-                        <button class="btn btn-outline-secondary btn-sm"  type="button" v-if="labelEdit" v-on:click="labelProtoOkClick()">OK</button>
+                        <button class="btn btn-outline-secondary btn-sm"  type="button" v-if="labelEdit" v-on:click="labelProtoOkClick()"><i class="fas fa-check"></i></button>
                     </div>
                 </div>
 
                 <div class="form-control form-control-sm" v-if="!labelEdit" v-on:click="labelClick()"> {{position.label}} </div>
 
             </td>
-            <td class="w-12">
-                <p> {{Math.abs(position.actual)}} </p>
+            <td class="w-20">
+
+                <div class="form-control form-control-sm" v-if="!actualEdit" v-on:click="actualClick()"> {{Math.abs(position.actual)}} </div>
+
             </td>
-            <td class="w-12">
-                <p> {{Math.abs(position.planned)}} </p>
+            <td class="w-20">
+
+                <div class="form-control form-control-sm"> {{Math.abs(position.planned)}} </div>
+                
             </td>
         </tr>
     `,
@@ -49,6 +53,9 @@ Vue.component('budget-position', {
             this.position.label = this.labelProto;
             this.labelEdit = false;
         },
+        actualClick: function () {
+
+        },
         dismissEdit: function () {
             this.labelProto = this.position.label;
             this.labelEdit = false;
@@ -61,14 +68,14 @@ Vue.component('budget-position', {
 });
 
 Vue.component('budget-positions', {
-    template: `
-    <table class="table table-striped table-sm">
+    template: /*html*/ `
+    <table class="table table-sm">
         <thead>
         <tr>
             <th scope="col"></th>
-            <th scope="col">Label</th>
-            <th scope="col">Actual</th>
-            <th scope="col">Planned</th>
+            <th scope="col">Tytuł</th>
+            <th scope="col">Obecnie</th>
+            <th scope="col">Wartość</th>
         </tr>
         </thead>
         <tbody>   
@@ -90,19 +97,29 @@ Vue.component('budget-positions', {
 })
 
 const Budgets = { 
-    template: `
-    <div class="container">
+    template: /*html*/ `
+    <div class="container-fluid">
         <h4> Budget positions </h4>
         <div class="row">
-            <div class="col-6">
-                <budget-positions
-                    v-bind:positions="positivePositions"
-                ></budget-positions>
+            <div class="col-md-6">
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <h5 class="card-title">Wpływy</h5>
+                        <budget-positions
+                            v-bind:positions="positivePositions"
+                        ></budget-positions>
+                    </div>
+                </div>
             </div> 
-            <div class="col-6">
-                <budget-positions
-                    v-bind:positions="negativePositions"
-                ></budget-positions>
+            <div class="col-md-6">
+                <div class="card mb-2">
+                    <div class="card-body">
+                        <h5 class="card-title">Wypływy</h5>
+                        <budget-positions
+                            v-bind:positions="negativePositions"
+                        ></budget-positions>
+                    </div>
+                </div>
             </div>    
         </div>
     </div>

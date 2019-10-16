313702b9baffdca5d24d004311c05e3ca24c13cb
tzwierzy <T.Zwierzynski@wasko.pl>
2019-10-16 16:01:22 +0200

    dodano style

--- webapp/components/budgets.js
+++ webapp/components/budgets.js
@@ -6,7 +6,13 @@ Vue.component('budget-position', {
                     <input class="form-check-input position-static" type="checkbox" v-model="position.active">
                 </div>
             </th>
-            <td class="w-75">
+            <td class="w-60">
+
+                <div class="form-control form-control-sm" v-bind:class="{ 'border-light': !position.active }" v-if="!labelEdit" v-on:click="labelClick()">
+                    <div>
+                        {{position.label}}
+                    </div>
+                </div>
 
                 <div class="input-group input-group-sm" v-if="labelEdit">
                     <input ref="labelInput" type="text" class="form-control form-control-sm" v-if="labelEdit" v-on:keyup.enter="labelProtoOkClick()" v-model="labelProto">
@@ -15,22 +21,46 @@ Vue.component('budget-position', {
                     </div>
                 </div>
 
-                <div class="form-control form-control-sm" v-if="!labelEdit" v-on:click="labelClick()"> {{position.label}} </div>
-
             </td>
             <td class="w-20">
 
-                <div class="form-control form-control-sm" v-if="!actualEdit" v-on:click="actualClick()"> {{Math.abs(position.actual)}} </div>
+                <div class="input-group input-group-sm"  v-if="!actualEdit">
+                    <div class="form-control form-control-sm" v-bind:class="{ 'border-light': !position.active }" v-on:click="actualClick()">
+                        <div v-bind:class="{ 'text-success': config.positive, 'text-danger': !config.positive }">    
+                            {{position.actual}}
+                        </div>
+                    </div>
+                    <div class="input-group-append" v-bind:class="{ 'border-light': !position.active }">
+                        <button class="btn btn-outline-secondary btn-sm" type="button" v-bind:class="{ 'border-light': !position.active }" v-on:click="bumpActual()"><i class="fas fa-level-up-alt"></i></button>
+                    </div>
+                </div>
 
+                <div class="input-group input-group-sm" v-if="actualEdit">
+                    <input ref="actualInput" type="number" class="form-control form-control-sm" v-on:keyup.enter="actualProtoOkClick()" v-model="actualProto">    
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary btn-sm" type="button" v-on:click="actualProtoOkClick()"><i class="fas fa-check"></i></button>
+                    </div>
+                </div>
             </td>
             <td class="w-20">
 
-                <div class="form-control form-control-sm"> {{Math.abs(position.planned)}} </div>
+                <div class="form-control form-control-sm" v-bind:class="{ 'border-light': !position.active }" v-if="!plannedEdit" v-on:click="plannedClick()">
+                    <div v-bind:class="{ 'text-success': config.positive, 'text-danger': !config.positive }">
+                        {{position.planned}}
+                    </div>
+                </div>
+
+                <div class="input-group input-group-sm" v-if="plannedEdit">
+                    <input ref="plannedInput" type="number" class="form-control form-control-sm" v-on:keyup.enter="plannedProtoOkClick()" v-model="plannedProto">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary btn-sm" type="button" v-on:click="plannedProtoOkClick()"><i class="fas fa-check"></i></button>
+                    </div>
+                </div>
                 
             </td>
         </tr>
     `,
-    props: [ 'position' ],
+    props: [ 'position', 'config' ],
     data: function () {
         return {
             labelProto: this.position.label,
@@ -43,6 +73,7 @@ Vue.component('budget-position', {
     },
     methods: {
         labelClick: function () {
+            this.dismissEdit();
             this.labelEdit = true;
             Vue.nextTick( () => {
                 this.$refs.labelInput.focus();
@@ -54,7 +85,36 @@ Vue.component('budget-position', {
             this.labelEdit = false;
         },
         actualClick: function () {
-
+            this.dismissEdit();
+            this.actualEdit = true;
+            Vue.nextTick( () => {
+                this.$refs.actualInput.focus();
+                this.$emit('editing');
+            });
+        },
+        actualProtoOkClick: function () {
+            if ( this.actualProto > this.position.planned ) {
+                this.position.planned = this.actualProto;
+                this.plannedProto = this.actualProto;
+            }
+            this.position.actual = this.actualProto;
+            this.actualEdit = false;
+        },
+        plannedClick: function () {
+            this.dismissEdit();
+            this.plannedEdit = true;
+            Vue.nextTick( () => {
+                this.$refs.plannedInput.focus();
+                this.$emit('editing');
+            });
+        },
+        plannedProtoOkClick: function () {
+            this.position.planned = this.plannedProto;
+            this.plannedEdit = false;
+        },
+        bumpActual: function () {
+            this.position.actual = this.position.planned;
+            this.actualProto = this.plannedProto;
         },
         dismissEdit: function () {
             this.labelProto = this.position.label;
@@ -69,54 +129,93 @@ Vue.component('budget-position', {
 
 Vue.component('budget-positions', {
     template: /*html*/ `
-    <table class="table table-sm">
-        <thead>
-        <tr>
-            <th scope="col"></th>
-            <th scope="col">Tytuł</th>
-            <th scope="col">Obecnie</th>
-            <th scope="col">Wartość</th>
-        </tr>
-        </thead>
-        <tbody>   
-            <budget-position
-                ref="budgetPosition"
-                v-for="(position, index) in positions"
-                v-bind:key="position.id"
-                v-bind:position="position"
-                v-on:editing="$refs.budgetPosition.forEach( (b, i) => {if (index !== i) { b.dismissEdit(); } } )"
-            ></budget-position>
-        </tbody>
-    </table>
+    <div>
+        <table class="table table-sm">
+            <thead>
+            <tr>
+                <th scope="col"></th>
+                <th scope="col">Tytuł</th>
+                <th scope="col">Obecnie</th>
+                <th scope="col">Wartość</th>
+            </tr>
+            </thead>
+            <tbody>   
+                <budget-position
+                    ref="budgetPosition"
+                    v-for="(position, index) in positions"
+                    v-bind:key="position.id"
+                    v-bind:position="position"
+                    v-bind:config="getPositionConfig()"
+                    v-on:editing="$refs.budgetPosition.forEach( (b, i) => { if (index !== i) { b.dismissEdit(); } } )"
+                ></budget-position>
+            </tbody>
+        </table>
+
+        <div class="position-bottom mb-2">
+            Suma: {{actualSum}} Total: {{plannedSum}}
+        </div>
+
+    </div>
     `,
-    props: [ 'positions' ],
+    props: [ 'positions', 'config' ],
     data: function () {
         return {
         };
+    },
+    computed: {
+        actualSum: function () {
+            let actualSum = 0;
+            this.positions.forEach( position => {
+                if ( position.active ) {
+                    actualSum = actualSum + parseInt(position.actual);
+                }
+            });
+            return actualSum;
+        },
+        plannedSum: function () {
+            let plannedSum = 0;
+            this.positions.forEach( position => {
+                if ( position.active ) {
+                    plannedSum = plannedSum + parseInt(position.planned);
+                }
+            });
+            return plannedSum;
+        }
+    },
+    methods: {
+        getPositionConfig: function () {
+            if ( this.config.positive ) {
+                return { positive: true };
+            } else {
+                return { positive: false };
+            }
+        }
     }
-})
+});
 
 const Budgets = { 
     template: /*html*/ `
     <div class="container-fluid">
         <h4> Budget positions </h4>
         <div class="row">
-            <div class="col-md-6">
-                <div class="card mb-2">
+            <div class="col-lg-6">
+                <div class="card mb-2 h-100">
                     <div class="card-body">
                         <h5 class="card-title">Wpływy</h5>
                         <budget-positions
                             v-bind:positions="positivePositions"
+                            v-bind:config="{ positive: true }"
                         ></budget-positions>
                     </div>
                 </div>
             </div> 
-            <div class="col-md-6">
-                <div class="card mb-2">
+            <div class="col-lg-6">
+                <div class="card mb-2 h-100">
                     <div class="card-body">
                         <h5 class="card-title">Wypływy</h5>
                         <budget-positions
                             v-bind:positions="negativePositions"
+                            v-bind:config="{ positive: false }"
                         ></budget-positions>
                     </div>
                 </div>
@@ -127,17 +226,21 @@ const Budgets = {
     data: function () {
         return {
             positivePositions: [],
-            negativePositions: []
+            negativePositions: [],
+            positiveConfig: { positive: true },
+            negativeConfig: { positive: false }
         }
-    },
+    }, // TODO: przyzapisywaniu do bazy pamietac, zeby negatywne zapisac z minusem
     created: function () {
         let positions = getBudget0().positions;
         positions.forEach( pos => {
-            if (pos.actual >= 0) {
+            if (pos.planned >= 0) {
                 this.positivePositions.push( pos );
             } else {
                 this.negativePositions.push( pos );
             }
+            pos.planned = Math.abs(pos.planned);
+            pos.actual = Math.abs(pos.actual);
         });
     }
 };
